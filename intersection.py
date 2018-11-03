d03baa95ef230b94358ab597abb7ef575f384e24
David Teresi <dkteresi+github@gmail.com>
2018-11-03 01:09:36 -0400

    add rails to Intersection

--- intersection.py
+++ intersection.py
@@ -1,8 +1,10 @@
 from typing import List
 
 from car import Car
+from rail import Rail
 
 
 class Intersection:
-    def __init__(self, cars: List[Car]):
+    def __init__(self, cars: List[Car], rails: List[Rail]):
         self.cars = cars
+        self.rails = rails
