b48b4c2e1578b80ef82beaf839c3b77d48686cd2
Justin <justin.smeal@mail.utoronto.ca>
2018-11-03 01:33:06 -0400

    Starting to detect collisions

--- intersection.py
+++ intersection.py
@@ -1,3 +1,11 @@
 class Intersection:
     def __init__(self, cars):
         self.cars = cars
+        self.init_collisions_dict()
+
+    def init_collisions_dict(self):
+        self.collisions_dict = {}
+
+    def collision(self, index_1, index_2):
+        car_1 = self.cars[index_1]
+        car_2 = self.cars[index_2]
