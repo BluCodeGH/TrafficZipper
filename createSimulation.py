eda665e99d8342e8298ca3a6333b2dc16b925ace
Jordan Ruetz <jordan.ruetz@mail.utoronto.ca>
2018-11-03 17:58:11 -0400

    way to create an intersection from file

--- createSimulation.py
+++ createSimulation.py
@@ -1,8 +1,40 @@
+from rail import *
+from intersection import *
+from car import *
+
+
+def getRail(transform, value):
+    if value == 0:
+        return LeftRail(transform)
+    elif value == 1:
+        return StraightRail(transform)
+    else:
+        return RightRail(transform)
+
+# cars data is formatted speed,railNum,priority
+# rails data is formatted transform,type
 if __name__ == "__main__":
-    file = open("rails.txt")
-    data = file.readlines()
+    rails_dict = {1: ""}
+
+    railsFile = open("assets/rails.txt")
+    railsData = railsFile.readlines()
+
+    carsFile = open("assets/cars.txt")
+    carsData = carsFile.readlines()
 
     rails = []
-    for item in data:
-        item.split(",")
-        print(item)
+    for item in railsData:
+        item = item.strip()
+        value = item.split(",")
+
+        rails.append(getRail(value[0], value[1]))
+
+    cars = []
+    for item in carsData:
+        item = item.strip()
+        value = item.split(",")
+
+        cars.append(Car(int(value[0]), rails[int(value[1]) - 1], int(value[2])))
+
+    intersection = Intersection(cars, rails)
+    intersection.update()
