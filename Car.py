1f0c14becbf6088d448dc94eee26cf37aeeadfb2
Justin <justin.smeal@mail.utoronto.ca>
2018-11-03 00:29:30 -0400

    Got rid of direction property of cars

--- Car.py
+++ Car.py
@@ -1,9 +1,8 @@
 class Car:
-    def __init__(self, length, width, initial_speed, direction, priority):
+    def __init__(self, length, width, initial_speed, priority):
         self.length = length
         self.width = width
         self.speed = initial_speed
-        self.direction = direction
         # The car's position is the scalar representing how far along the rail we are.
         self.position = 0
         self.priority = priority
